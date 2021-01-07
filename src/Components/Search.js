beea8b892854568e408adcbc35844dacef14ce1e
ManikandanRamSaro <mani95ram@outlook.com>
2021-01-07 10:31:55 +0530

    before start working with api works - 7-1-21

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,11 +2,13 @@ import React,{useState,useEffect} from 'react';
 
 const Search = () =>
 {
+    const [term,setTerm]= useState('');
+    
     useEffect(()=>{  // this like component did mount event and its works in 3, 1 is without 2 param, 2 is [] empty array param, 3 is given below
         console.log('term get changed')
     },[term])
 
-    const [term,setTerm]= useState('');
+   
     return (<div>
         <div className="ui form">
             <div className="field">
