6b44c04dd91dfcd528a91d01c6af0aa25d25f1c2
ManikandanRamSaro <mani95ram@outlook.com>
2021-01-07 14:41:22 +0530

    after axios issues identified and started working on list rendering - 7-1-21

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,12 +1,31 @@
 import React,{useState,useEffect} from 'react';
-
+import axios from 'axios';
 const Search = () =>
 {
     const [term,setTerm]= useState('');
-    
-    useEffect(()=>{  // this like component did mount event and its works in 3, 1 is without 2 param, 2 is [] empty array param, 3 is given below
-        console.log('term get changed')
-    },[term])
+    const [result,setResult]=useState([]);
+        // this like component did mount event and its works in 3, 1 is without 2 param, 2 is [] empty array param, 3 is given below
+  
+    useEffect(()=>{ 
+         
+         const responseGet = async () => {
+            const {data} =  await axios.get('https://en.wikipedia.org/w/api.php',{
+                 params:{
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch:term,
+                 },
+
+             });
+             setResult(data.query.search);
+             console.log(data.query.search)
+         }; 
+         if(term){
+            responseGet();
+         }
+    },[term]);
 
    
     return (<div>
