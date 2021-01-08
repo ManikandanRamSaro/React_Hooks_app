2ee9d32361350a74c8d64129edb62b4c09ed506a
ManikandanRamSaro <mani95ram@outlook.com>
2021-01-08 19:27:47 +0530

    worked and completed use of useRef - 8-1-21

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState , useEffect,useRef } from 'react';
 
 const Dropdown = ({options,selected,onSelectedChange}) =>{
+
+    const [open,setOpen] =useState(false);
+    const ref=useRef();
+
+    useEffect(()=>{
+        document.body.addEventListener('click',(event)=>{ 
+             if(ref.current.contains(event.target))
+             {
+                 return;
+             }
+             setOpen(false);
+        });
+    },[]);
+
     const renderedOption = options.map((opt)=>{
         if(opt.value===selected.value){
             return null;
@@ -12,15 +26,20 @@ const Dropdown = ({options,selected,onSelectedChange}) =>{
             >
                 {opt.label}
             </div>);
-    })
+    });
+
+
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label"> Select a Value</label>
-                <div className="ui selection dropdown visible active">
+                <div 
+                    onClick={()=>{setOpen(!open)}}
+                    className={`ui selection dropdown ${ open ? 'visible active' : ''} `}
+                    >
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
-                    <div className="menu visible transition">
+                    <div className={`menu  ${ open ? 'visible transition' : ''}`}>
                          {renderedOption}
                     </div>               
                 </div>
