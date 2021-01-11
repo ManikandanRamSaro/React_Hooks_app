9e472aafb827da87633ad1082ecd946a767d4fe5
ManikandanRamSaro <mani95ram@outlook.com>
2021-01-11 11:46:37 +0530

    After all task completed and before start of navigation - 11-1-21

--- src/Components/Convert.js
+++ src/Components/Convert.js
@@ -4,25 +4,37 @@ import axios from 'axios';
 const Convert = ({language,text}) =>{
 
     const [converted,setConverted]= useState('');
+    
+    const [deBouncetext,setDeBouncetext]= useState(text);
+
+    useEffect(()=>{
+        const debouncetimer = setTimeout(() => {
+            setDeBouncetext(text);
+        }, 500);
+
+        return () =>{
+            clearTimeout(debouncetimer);   
+        }
+    },[text]);
+
+
     useEffect(()=>{
 
         const doTranslate = async () =>{
             const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2',{},{
-                params:{
-                    q:text,
-                    target:language.value,
-                    key:'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
-    
-                }
-    
-                }
-             
+                    params:{
+                        q:deBouncetext,
+                        target:language.value,
+                        key:'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+        
+                    }
+                }             
             );
             setConverted(data.data.translations[0].translatedText)
         };
         doTranslate();
         
-    },[language,text]);
+    },[language,deBouncetext]);
 
     return <div><h1>{converted}</h1></div>;
 }
